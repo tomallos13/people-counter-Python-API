92154ffc80133e5bfc389e5fc6a0249217c942a9
Tomasz Zelias <tomallos13@gmail.com>
2024-01-28 10:41:09 +0100

    actions

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
 
 api.add_resource(HelloWorld, '/test')
 
